2cfb7d63fe5e834c5b2c6c2453ef48a511964d04
Naomi <33360546+UruvielNaomi@users.noreply.github.com>
2023-03-07 10:13:25 +0100

    Updates JS
    worked on interval function, started working on the select city function.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
+
+setInterval(function () {
 //Berlin
 let berlinElement = document.querySelector("#berlin");
 let berlinDateElement = berlinElement.querySelector(".date");
 let berlinTimeElement = berlinElement.querySelector(".time-stamp");
+let berlinTimeZone = moment().tz("Europe/Berlin");
 
-berlinDateElement.innerHTML = moment().format("MMMM Do YYYY")
-berlinTimeElement.innerHTML = moment().format("HH:mm:ss");
+berlinDateElement.innerHTML = berlinTimeZone.format("MMMM Do YYYY")
+berlinTimeElement.innerHTML = berlinTimeZone.format("HH:mm:ss");
+}, 1000);
 
-
+setInterval(function () {
 // London
 let londonElement = document.querySelector("#london");
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time-stamp");
+let londonTimeZone = moment().tz("Europe/London");
 
-londonDateElement.innerHTML = moment().format("MMMM Do YYYY")
-londonTimeElement.innerHTML = moment().format("HH:mm:ss");
+londonDateElement.innerHTML = londonTimeZone.format("MMMM Do YYYY")
+londonTimeElement.innerHTML = londonTimeZone.format("HH:mm:ss");
+}, 1000);
+
+
+
+
+function showDateTime(event){
+    if (event.target.value.length > 0) {
+        let currentTime = moment().tz(event.target.value).format("MMMM Do YYYY")
+        
+    }
+}
+
+let selectElement = document.querySelector("#cities");
+selectElement.addEventListener("change", showDateTime);
